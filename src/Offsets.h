613dd9df638dd0c46fbc4ca1cdaae705ea213869
ac is ud <66012875+ac-1337@users.noreply.github.com>
2022-07-05 22:21:29 +0300

    Update Offsets.h

--- src/Offsets.h
+++ src/Offsets.h
@@ -6,6 +6,6 @@ namespace gOffsets
 {
 	constexpr SIZE_T SharedUserInfo = 0x35AA79CUL;
 	constexpr SIZE_T SharedGameManager = 0x34E2238UL;
-	constexpr SIZE_T CUE_PROPERTIES_MAX_POWER = 0x34E2238UL;
-	constexpr SIZE_T CUE_PROPERTIES_SPIN = 0x34E2238UL;
+	constexpr SIZE_T CUE_PROPERTIES_MAX_POWER = 0x35685D0UL;
+	constexpr SIZE_T CUE_PROPERTIES_SPIN = 0x35685D0UL;
 }
